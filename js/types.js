11ff9111579a426ab7006aaf2a8d848805a9cecc
Dmitry Myadzelets <DmitryMyadzelets@gmail.com>
2015-01-09 01:03:47 +0100

    Initial commit

---
+++ js/types.js
@@ -0,0 +1,41 @@
+
+var basicDataTypes = [
+    "boolean",
+    "timestamp",
+    "double",
+    "character",
+    "string",
+    "list"
+];
+
+
+// The list of objects defined in
+// https://github.com/FamilySearch/gedcomx/blob/master/specifications/support/conceptual-model-graph.pdf
+
+var objects = [
+    "Address",
+    "Agent",
+    "Attribution",
+    "Date",
+    "Document",
+    "Event",
+    "EventRole",
+    "EvidenceReference",
+    "Fact",
+    "Gender",
+    "Name",
+    "NameForm",
+    "NamePart",
+    "Note",
+    "OnlineAccount",
+    "Person",
+    "PlaceReference",
+    "Qualifier",
+    "Relationship",
+    "SourceCitation",
+    "SourceDescription",
+    "SourceReference",
+    "Subject",
+    "TextValue"
+];
+
